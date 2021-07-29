52c27f418d06f28e6780855f749ee6759fd8e4cc
Victor <monsieurviktor@gmail.com>
2021-07-29 15:21:37 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ export default App;
   TodoApp - for the display
   should import the following
     -todoform- for the form to add
-    -todoedit- to edit and save the todoform
+        -todo- for the individual todos
+      -todoedit- to edit and save the todoform
     -todolist- shows the list, mapping out the ticked todo
-    -todo- for the individual todos
 
     each todo: id, task: what the todo is, completed: true or false
 */}
